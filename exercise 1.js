8769cdd6d50ff923f203c81ff115f62318aef2ff
Samantha Cummings <scummings1219@gmail.com>
2022-02-17 21:53:33 -0500

    loop exercise1

---
+++ exercise 1.js
@@ -0,0 +1,17 @@
+let nameone ="Samantha";
+for (let i = 0; nameone.length < 15; i++) {
+nameone = " " + nameone;
+console.log(nameone);
+}
+
+
+//2 to 100, evens only
+for (let i=2; i<=100; i+=2){ //or you can do i= i+2
+console.log(i)
+}
+
+for (let i=2; i<=100; i+=2){ //or you can do i= i+2
+    if (i % 2 === 0)
+    console.log(i)
+    }
+   // modulus example above  
---
+++ scripts.js
@@ -0,0 +1,54 @@
+//for 1 to 10
+/*for (let i= 1; i <= 10; i++){
+console.log(i);
+}*/
+
+
+//for 10 to 1
+/*for (let i= 10; i >= 0; i--){
+    console.log(i);
+    }*/
+
+
+//while loop 1 to 10    
+/*let i=1;
+while(i<10){
+console.log(i);
+i++;
+}
+    */
+   //while loop 10 to 1
+   /*let i=10;
+   while (i>=1){
+       console.log(i):
+       i--;
+   }*/
+
+//do.. while loop 1 to 10
+/*let count=1;
+do {
+    console.log(count);
+    count++;
+}
+while( count <= 10)*/
+
+
+//do...while 10 to 1
+/*let i=10;
+do{
+    console.log(i);
+    i--;
+}
+while (i>=1)
+*/
+
+
+
+
+// const numbersfor...of loop
+/*const numbers =[1,2,3,4,5,6,7,8,9,10];
+for (let number of numbers){
+    console.log(number);
+}
+*/
+
